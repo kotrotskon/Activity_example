d842bd42eba3eaee169ecb8ebc785391e4726de5
KotrotsKon <kotrotskon@users.noreply.github.com>
2022-06-30 20:38:24 +0300

    Android Life Circle

--- app/src/main/java/com/example/activity_example/MainActivity.java
+++ app/src/main/java/com/example/activity_example/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("LIFE_CIRCLE", "MainActivity - onCreate()");
         setContentView(R.layout.activity_main);
 
         EditText editText = findViewById(R.id.edTxt);
@@ -28,5 +30,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFE_CIRCLE", "MainActivity - onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFE_CIRCLE", "MainActivity - onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LIFE_CIRCLE", "MainActivity - onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIFE_CIRCLE", "MainActivity - onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LIFE_CIRCLE", "MainActivity - onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LIFE_CIRCLE", "MainActivity - onRestart()");
     }
 }
--- app/src/main/java/com/example/activity_example/SecondActivity.java
+++ app/src/main/java/com/example/activity_example/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.activity_example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("LIFE_CIRCLE", "SecondActivity - onCreate()");
         setContentView(R.layout.activity_second);
 
         TextView textView = findViewById(R.id.txtVw);
@@ -17,5 +19,41 @@ public class SecondActivity extends AppCompatActivity {
         String text = getIntent().getExtras().getString("myText");
 
         textView.setText(text);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFE_CIRCLE", "SecondActivity - onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFE_CIRCLE", "SecondActivity - onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LIFE_CIRCLE", "SecondActivity - onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIFE_CIRCLE", "SecondActivity - onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LIFE_CIRCLE", "SecondActivity - onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LIFE_CIRCLE", "SecondActivity - onRestart()");
     }
 }
